7d527614a860fa511137a537a57b2173ca19e0a5
jerseyg <Jerseyg@shaw.ca>
2012-08-30 08:42:41 -0500

    Copy and Pasted From A1 Appendix

---
+++ Assignment1/example.rb
@@ -0,0 +1,37 @@
+orders_by_customer_id = {
+        101 => [
+           { :product_id => 13, :quantity => 2  },
+           { :product_id => 35, :quantity => 16 },
+           { :product_id => 11, :quantity => 3  }],
+        24 => [
+           { :product_id => 11, :quantity => 3  },
+           { :product_id => 35, :quantity => 4  }],
+        36 => [
+           { :product_id => 25, :quantity => 17 },
+           { :product_id => 42, :quantity => 2  },
+           { :product_id => 35, :quantity => 7  }]
+}
+
+products_by_id = {
+        11 => { :name => "teddy bear", :price => 14.23 },
+        25 => { :name => "toy ghosts", :price => 4.34  },
+        13 => { :name => "giant newt", :price => 56.00 },
+        42 => { :name => "sandcastle", :price => 12.45 },
+        35 => { :name => "shoe phone", :price => 86.00 }
+}
+
+customers_by_id = {
+        101 => { :name => 'John Smith', :address => '128 Good St.', :city => 'Winnipeg', :province => 'MB' },
+        24  => { :name => 'Ralph Woodhorse', :address => '67 Pylon Way', :city => 'Calgary', :province => 'AB' },
+        36  => { :name => 'Mary Contra', :address => '342 Modem Drive', :city => 'Regina', :province => 'SK' }
+}
+
+Sales_tax_by_province = { 'MB' => 0.07, 'AB' => 0, 'BC' => 0.07, 'SK' => 0.05 }
+
+GST = 0.05
+
+def currency amount
+ sprintf("$%.2f",amount)
+end
+
+
