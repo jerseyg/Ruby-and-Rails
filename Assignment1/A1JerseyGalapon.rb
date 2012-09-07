a3dc4f2f7bcb5dda439cec5cceea67289a41027f
jerseyg <jerseyg@shaw.ca>
2012-09-07 10:40:39 -0500

    Changed to .each

--- Assignment1/A1JerseyGalapon.rb
+++ Assignment1/A1JerseyGalapon.rb
@@ -1,29 +1,29 @@
 orders_by_customer_id = {
-        101 => [
-           { :product_id => 13, :quantity => 2  },
-           { :product_id => 35, :quantity => 16 },
-           { :product_id => 11, :quantity => 3  }],
-        24 => [
-           { :product_id => 11, :quantity => 3  },
-           { :product_id => 35, :quantity => 4  }],
-        36 => [
-           { :product_id => 25, :quantity => 17 },
-           { :product_id => 42, :quantity => 2  },
-           { :product_id => 35, :quantity => 7  }]
+  101 => [
+    { :product_id => 13, :quantity => 2  },
+    { :product_id => 35, :quantity => 16 },
+    { :product_id => 11, :quantity => 3  }],
+  24 => [
+    { :product_id => 11, :quantity => 3  },
+    { :product_id => 35, :quantity => 4  }],
+  36 => [
+    { :product_id => 25, :quantity => 17 },
+    { :product_id => 42, :quantity => 2  },
+    { :product_id => 35, :quantity => 7  }]
 }
 
 products_by_id = {
-        11 => { :name => "teddy bear", :price => 14.23 },
-        25 => { :name => "toy ghosts", :price => 4.34  },
-        13 => { :name => "giant newt", :price => 56.00 },
-        42 => { :name => "sandcastle", :price => 12.45 },
-        35 => { :name => "shoe phone", :price => 86.00 }
+  11 => { :name => "teddy bear", :price => 14.23 },
+  25 => { :name => "toy ghosts", :price => 4.34  },
+  13 => { :name => "giant newt", :price => 56.00 },
+  42 => { :name => "sandcastle", :price => 12.45 },
+  35 => { :name => "shoe phone", :price => 86.00 }
 }
 
 customers_by_id = {
-        101 => { :name => 'John Smith', :address => '128 Good St.', :city => 'Winnipeg', :province => 'MB' },
-        24  => { :name => 'Ralph Woodhorse', :address => '67 Pylon Way', :city => 'Calgary', :province => 'AB' },
-        36  => { :name => 'Mary Contra', :address => '342 Modem Drive', :city => 'Regina', :province => 'SK' }
+  101 => { :name => 'John Smith', :address => '128 Good St.', :city => 'Winnipeg', :province => 'MB' },
+  24  => { :name => 'Ralph Woodhorse', :address => '67 Pylon Way', :city => 'Calgary', :province => 'AB' },
+  36  => { :name => 'Mary Contra', :address => '342 Modem Drive', :city => 'Regina', :province => 'SK' }
 }
 
 Sales_tax_by_province = { 'MB' => 0.07, 'AB' => 0, 'BC' => 0.07, 'SK' => 0.05 }
@@ -31,43 +31,46 @@ Sales_tax_by_province = { 'MB' => 0.07, 'AB' => 0, 'BC' => 0.07, 'SK' => 0.05 }
 GST = 0.05
 
 def currency amount
- sprintf("$%.2f",amount)
+  sprintf("$%.2f",amount)
 end
 
+orders_by_customer_id.each do |cust_id, orders|
+#init subtotal outside the orders.each block
+  subtotal = 0
 
-$i = 0
-#Added key arrays
-custs = customers_by_id.keys
-orderid = orders_by_customer_id 
-orders = orderid.keys
-products = products_by_id.keys
-#bunch of periods... 
-BOP = " ...................... "
-while $i < custs.length do
- 
-  
-  
-  puts "Invoice for #{customers_by_id[custs[$i]][:name]}"
-  puts customers_by_id[custs[$i]][:address]
-  puts "#{customers_by_id[custs[$i]][:city]} #{customers_by_id[custs[$i]][:province]}"
+  #put customer information into variables for simple puts
+  customer_name = customers_by_id[cust_id][:name]
+  customer_address = customers_by_id[cust_id][:address]
+  customer_city = customers_by_id[cust_id][:city]
+  customer_province = customers_by_id[cust_id][:province]
+
   puts ""
-  
-  if $i == 1
-   item_desc = orderid[orders[$i]][0][:product_id]
-   item_desc2 = orderid[orders[$i]][1][:product_id]
-      puts "#{products_by_id[item_desc][:name]} #{BOP}" + 
-           "#{orderid[orders[$i]][0][:quantity]} * #{currency(products_by_id[item_desc][:price])}"
-      puts "#{products_by_id[item_desc2][:name]} #{BOP}" + 
-           "#{orderid[orders[$i]][1][:quantity]} * #{currency(products_by_id[item_desc2][:price])}"
-  else
-   item_desc = orderid[orders[$i]][0][:product_id]
-   item_desc2 = orderid[orders[$i]][1][:product_id]
-   item_desc3 = orderid[orders[$i]][2][:product_id]
-      puts "#{products_by_id[item_desc][:name]} #{BOP} #{orderid[orders[$i]][0][:quantity]} * #{currency(products_by_id[item_desc][:price])}"
-      puts "#{products_by_id[item_desc2][:name]} #{BOP} #{orderid[orders[$i]][1][:quantity]} * #{currency(products_by_id[item_desc2][:price])}"
-      puts "#{products_by_id[item_desc3][:name]} #{BOP} #{orderid[orders[$i]][2][:quantity]} * #{currency(products_by_id[item_desc3][:price])}"
-  end
-  
-  $i += 1
-end
+  puts "Invoice for #{customer_name}"
+  puts customer_address
+  puts "#{customer_city}, #{customer_province}"
+  puts ""
 
+  orders.each do |order|
+  #put product information into variables for simple puts
+    order_amount = order[:quantity]
+    products = products_by_id[order[:product_id]][:name]
+    product_price = products_by_id[order[:product_id]][:price]
+    item_price = order_amount * product_price
+
+    puts "#{products} .......................... #{order_amount} x #{currency(product_price)} = #{currency(item_price)}"
+    subtotal += item_price
+
+  end
+
+  p_tax = subtotal * Sales_tax_by_province['MB']
+  g_tax = subtotal * GST
+  grand_total = subtotal + p_tax + g_tax
+  #rounded_p_tax variable is for using the .round(2)
+  rounded_p_tax = Sales_tax_by_province['MB'] * 100
+
+  puts ""
+  puts "Sub Total                : #{currency(subtotal)}"
+  puts "Provincial Tax (#{rounded_p_tax.round(2)}%)   : #{currency(p_tax)} "
+  puts "Gross Sales Tax (#{GST* 100}%)  : #{currency(g_tax)}"
+  puts "Grand Total              : #{currency(grand_total)}"
+end
