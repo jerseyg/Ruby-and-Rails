85e2960c01c5c566ddfa56126fe6523c1e051fcd
jerseyg <jerseyg@shaw.ca>
2012-09-13 10:42:27 -0500

    Protected method not made

--- UltimateChallenges/U3/OORuby.rb
+++ UltimateChallenges/U3/OORuby.rb
@@ -1,3 +1,17 @@
+require 'date'
+def parse_date(date)
+  the_date = Time.now
+  dates = Date.strptime(date, '%d-%m-%Y')
+   y = dates.year
+   m = dates.month
+   d =dates.day
+   age = the_date.year - y
+   age -=1 if the_date.month < m
+   age
+end
+
+
+
 class Person
   def initialize (first_name, last_name, dob)
     @first_name = first_name
@@ -10,14 +24,17 @@ class Person
 end
 
 class Instructor < Person
-  def initialize (hire_date)
+  def initialize (first_name, last_name, dob, hire_date)
+    super(first_name, last_name, dob)
     @hire_date = hire_date
     #Counter
     @@Instructor_count = defined?(@@Instructor_count) ? @@Instructor_count + 1 : 1
   end
-
   def self.Instructor_count
     @@Instructor_count
+  end
+  def to_s
+    "Instructor: #{first_name} #{last_name} dob: #{dob} (age: #{parse_date(dob)}), hired: #{hire_date} (#{parse_date(hire_date)} years go)"
   end
   attr_reader :hire_date
 end
@@ -34,8 +51,7 @@ class Student < Person
     @@Student_count
   end
   def to_s
-    @age = ""
-  "Student #{first_name} student number: #{@student_number}, Dob: #{dob} (age:#{@age}), Program: #{@program}"
+  "Student #{first_name} Student number: #{@student_number}, Dob: #{dob} (age:#{parse_date( dob)}), Program: #{@program}"
   end
 end
 
@@ -46,10 +62,13 @@ class Course
     #Counter
     @@Course_count = defined?(@@Course_count) ? @@Course_count + 1 : 1
   end
-
   def self.Course_count
     @@Course_count
   end
+  def to_s
+    "Course: #{@name}, Credit Hours: #{@credit_hours}"
+  end
+
 end
 
 class Section
@@ -59,16 +78,57 @@ class Section
     @term = term
     @section_number = section_number
     @students = students
+     @@Section_count = defined?(@@Section_count) ? @@Section_count + 1 : 1
   end
-
+  
+  def self.Section_count
+    @@Section_count
+  end
+  
   def self.add_student (student_number, program)
     Student.new(student_number, program)
   end
+  
+  def to_s
+    "#{@@Section_count}) Section number #{@section_number} of Course: #{@course} for term #{@term} has #{@students} students enrolled"
+  end
 end
 
+
+
+
+  
 students = Array.new
+instructors = Array.new
+courses = Array.new
+sections = Array.new
 #A few of these:
-students << Student.new('jersey', 'galapon', 12123, 23155, 'program')
+students << Student.new('jersey', 'galapon', '12-12-1991', 1997, 'BIT')
+students << Student.new('jersey', 'galapon', '12-12-1991', 1997, 'BIT')
+students << Student.new('jersey', 'galapon', '12-12-1991', 1997, 'BIT')
+students << Student.new('jersey', 'galapon', '12-12-1991', 1997, 'BIT')
+instructors << Instructor.new('Johnny', 'Appleseed', '12-1-1991', '12-02-2005')
+instructors << Instructor.new('Johnny', 'Appleseed', '12-1-1991', '12-02-2005')
+instructors << Instructor.new('Johnny', 'Appleseed', '12-1-1991', '12-02-2005')
+instructors << Instructor.new('Johnny', 'Appleseed', '12-1-1991', '12-02-2005')
+courses << Course.new('INTRO TO MATH', 3)
+courses << Course.new('INTRO TO MATH', 3)
+courses << Course.new('INTRO TO MATH', 3)
+courses << Course.new('INTRO TO MATH', 3)
+sections << Section.new(courses[1], instructors[1], 'W2010', 1, Student.Student_count)
+sections << Section.new(courses[1], instructors[1], 'W2010', 1, Student.Student_count)
+sections << Section.new(courses[1], instructors[1], 'W2010', 1, Student.Student_count)
+sections << Section.new(courses[1], instructors[1], 'W2010', 1, Student.Student_count)
 
 puts "There are #{Student.Student_count} Students:"
 students.each { |student| puts student.to_s }
+puts ""
+puts "There are #{Instructor.Instructor_count} Instructors:"
+instructors.each { |instructor| puts instructor.to_s}
+puts ""
+puts "There are #{Course.Course_count} Courses:"
+courses.each { |course| puts course }
+puts ""
+puts "There are #{Section.Section_count} Sections:"
+sections.each { |section| puts section }
+
