edb3a7fafc24f48c0ac472d8a5147829d31cd588
jerseyg <jerseyg@shaw.ca>
2012-09-13 10:42:00 -0500

    Don't know

--- Tried/salt.rb
+++ Tried/salt.rb
@@ -1,6 +1,31 @@
 require 'digest/md5'
 
-digest = Digest::MD5.hexdigest("Hello World\n")
-digest2 = Digest::SHA512.file
+$md5_array = Array.new
+$i = 0
 
-  puts digest2
+def Types_of_hash string
+
+md5 = Digest::MD5.hexdigest("#{string}")
+    
+  $md5_array << md5
+  #end
+ return $md5_array[$i]
+ 
+ #{(time_end - time)*1000} milliseconds", $md5_array[$i]
+ 
+
+
+end
+
+#Loop the number of times you want to create the digests. See how long it takes to create it.
+puts Types_of_hash "a"
+puts Types_of_hash "2"
+puts Types_of_hash "a"
+puts Types_of_hash "2"
+puts Types_of_hash "a"
+puts Types_of_hash "2"
+
+
+
+
+
